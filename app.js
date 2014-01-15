45e47960deda408dcbd0edffe2610d830a345eb4
pat-flew <pat-flew@users.noreply.github.com>
2014-01-15 17:04:55 +0000

    Improve matching in nodeIndexToCharIndex using fsm. Ignores comments and doesn't match elements that start with the same characters.

--- app.js
+++ app.js
@@ -9,17 +9,73 @@
 		charIndexToLocation,
 		processElement,
 		find;
-
-	// Take a node type and the index of this node (e.g. this is the 5th <li> element) and return the character index of the node
-	nodeIndexToCharIndex = function(html, nodeType, n) {
-		var i, index = 0;
-		nodeType = '<' + nodeType;
-		for (i = 0; i <= n; i += 1) {
-			index = html.indexOf(nodeType, index + 1);
+		
+	String.prototype.startsWith = function(str, i) {
+		if(this.substr(i, str.length) === str) {
+			return true;
+		} else {
+			return false;
 		}
-		return index;
 	};
 
+	// Take a node type and the index of this node (e.g. this is the 5th <li> element) and return the character index of the node
+	var nodeIndexToCharIndex = function(html, nodeType, n) {
+		var running = true,
+			i = 0, charIndex = -1,
+			nodeIndex = -1, nodeCount = 0,
+			state = 1,
+			openComment = '<!--', closeComment = '-->';
+
+		while(running) {
+			switch(state) {
+				case 1:
+					if(html.startsWith(openComment, i)) {
+						i += openComment.length;
+						state = 2;
+					} else if(html.startsWith('<' + nodeType, i)) {
+						nodeIndex = i;
+						i += nodeType.length + 1;
+						state = 3;
+					} else {
+						i++;
+					}
+				break;
+				case 2:
+					if(html.startsWith(closeComment, i)) {
+						i += closeComment.length;
+						state = 1;
+					} else {
+						i++;
+					}
+				break;
+				case 3:
+					if(html.startsWith(' ', i) || html.startsWith('>', i)) {
+						state = 4;
+					} else {
+						state = 1;
+						i++;
+					}
+				break;
+				case 4:
+					i++;
+					nodeCount++;
+					state = 1;
+				break;
+			}
+
+			if(nodeCount === n+1) {
+				charIndex = nodeIndex;
+				running = false;
+			}
+
+			if(i >= html.length) {
+				running = false;
+			}
+		}
+
+		return charIndex;
+	};
+	
 	// Take a string index as input and return the line and column number
 	charIndexToLocation = function(html, index, method) {
 		var substr = html.substr(0, index),
@@ -28,7 +84,7 @@
 			columnNumber = index - lastLineBreak;
 		return [lineNumber, columnNumber, method];
 	};
-
+	
 	// Calculate the location of an individual element
 	processElement = function($, html, $match, matchHtml) {
 		var matchHtmlLen = matchHtml.length,
